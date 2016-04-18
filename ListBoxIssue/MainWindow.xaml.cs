e5780be3e7729859f278bdd090d951428a1a6d05
Sebastian De Gambe <s.degambe@gmail.com>
2016-04-18 23:57:24 +0200

    R# hints

--- ListBoxIssue/MainWindow.xaml.cs
+++ ListBoxIssue/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ namespace ListBoxIssue
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    // ReSharper disable once RedundantExtendsListEntry
     public partial class MainWindow : Window
     {
         public MainWindow()
